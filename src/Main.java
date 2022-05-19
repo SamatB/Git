326ab3826f496e07bf84b1bc497985ee167ec056
samat <samhardy1927@gmail.com>
2022-05-19 19:31:35 +0300

    New car object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("BMW", 2000);
+        Car car2 = new Car("MB", 5000);
+        Car car3 = new Car("KIA", 3500);
     }
 }
